d0790fabad126f7ded4689cc96e89be639976d98
Yash <imyashpawar@gmail.com>
2023-04-13 12:06:12 +0530

    Added: Welcome message

---
+++ src/com/BridgeLabz/AdressBook_Main.java
@@ -0,0 +1,13 @@
+package com.BridgeLabz;
+
+public class AdressBook_Main
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Welcome To Adressbook System");
+
+
+    }
+
+}
