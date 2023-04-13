312d3e5021e2b1cf3d4ae90d7a1ebdf5eb1545ea
Yash <imyashpawar@gmail.com>
2023-04-13 13:28:31 +0530

    Added: Add new Contact and Maintain relation among classes

---
+++ src/com/BridgeLabz/Contact_person.java
@@ -0,0 +1,16 @@
+package com.BridgeLabz;
+
+public class Contact_person
+{
+     String Fname ;
+     String Lname;
+    String Address;
+    String CityName;
+    String StateName;
+    int ZipCode;
+    String PhoneNumber;
+    String email;
+
+
+
+}
